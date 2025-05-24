406a2313d17be62a8e27c7b4a120ccbccd0474ff
navi-04 <naveenrajthiyagarajan6@gmail.com>
2025-05-24 14:44:35 +0530

    IC-2:no changes, just for the check

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -9,7 +9,7 @@ import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router> 
       <Layout>
         <Routes>
           <Route path="/dashboard" element={<DashboardContainer />} />
